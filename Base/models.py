a967759ed2c17ffbf3ea40bb775efc4d7b3052f5
AlazarBekele <alazarthe70@gmail.com>
2024-10-24 21:41:07 +0300

    successfully deployed static file

--- Base/models.py
+++ Base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class WelcomePage (models.Model):
+
+  title = models.CharField (max_length=20)
+  picture = models.ImageField (upload_to='photo/')
+
+  def __str__(self) -> str:
+    return self.title
--- Base/views.py
+++ Base/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import WelcomePage
 
 # Create your views here.
 
 def index (request):
+
+  front_Img = WelcomePage.objects.filter().order_by('-title')[:2]
+
+  context = {
+
+    'front_Img' : front_Img
+
+  }
   
-  return render (request, 'index.html')
+  return render (request, 'index.html', context=context)
